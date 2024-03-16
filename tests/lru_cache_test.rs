b8a6ca3c5a48facd308e6c3531d56e4e687fcffa
jiisanda <jiisanda@protonmail.com>
2024-03-16 18:48:00 +0530

    tests to expiration

--- tests/lru_cache_test.rs
+++ tests/lru_cache_test.rs
@@ -1,10 +1,12 @@
 #[cfg(test)]
 mod tests {
     use pandas_pouch::lru::LRUCache;
+    use std::thread;
+    use std::time::Duration;
 
     #[test]
     fn test_lru_cache() {
-        let mut cache = LRUCache::new(2);
+        let mut cache = LRUCache::new(2, None); // default expiration of 3600 seconds
 
         cache.put(1, "a");
         assert_eq!(cache.get(1), Some("a"));
@@ -20,5 +22,19 @@ mod tests {
         assert_eq!(cache.get(2), None);
         assert_eq!(cache.get(3), Some("c"));
         assert_eq!(cache.get(4), Some("d"));
+    }
+
+    #[test]
+    fn test_lru_cache_with_expiration() {
+        let mut cache = LRUCache::new(2, Some(Duration::from_secs(2)));
+
+        cache.put(1, "a");
+        assert_eq!(cache.get(1), Some("a"));
+        cache.put(2, "b");
+        assert_eq!(cache.get(2), Some("b"));
+
+        thread::sleep(Duration::from_secs(5));
+        assert_eq!(cache.get(1), None);
+        assert_eq!(cache.get(2), None);
     }
 }
